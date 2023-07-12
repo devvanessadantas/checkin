217745f31e89ada65ed5449ac2d1153f1e71755c
devvanessadantas <dev.vanessa.dantas@gmail.com>
2023-07-11 21:59:46 -0300

    feature/002- To include configurations of BD and swagger.

---
+++ src/main/java/com/roses/checkin/person/registration/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.roses.checkin.person.registration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping
+    public void getCustomer(){
+
+    }
+}
